efc776a31b1f35a48e12237967566d7fc7839f91
Rick Ertl <ertl.rick@gmail.com>
2022-09-14 15:31:40 -0500

    basic display of city and temp by input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import "./style.scss";
+
+let location = "Chicago";
+let units = "imperial";
+const apiKey = "c421018a07c14061ff3b4e180dce400f";
+
+const form = document.querySelector("form");
+const locationName = document.querySelector(".location");
+const temperature = document.querySelector(".temperature");
+
+window.onload = function () {
+  getWeather(location);
+};
+
+form.addEventListener("submit", (event) => {
+  getWeather(form.querySelector("input").value);
+  form.reset();
+  event.preventDefault();
+});
+
+async function getWeather(location) {
+  try {
+    const response = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?" +
+        `q=${location}` +
+        `&units=${units}` +
+        `&appid=${apiKey}`,
+      { mode: "cors" }
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+    locationName.innerText = weatherData.name;
+    temperature.innerText = weatherData.main.temp;
+  } catch (error) {
+    console.log(error);
+  }
+}
